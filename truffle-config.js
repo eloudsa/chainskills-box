14c67cb1c99e2f237addd9978aff65bc686c4ab0
Said Eloudrhiri <info@noratek.net>
2018-12-03 23:49:51 +0100

    Configure the compiler

--- truffle-config.js
+++ truffle-config.js
@@ -7,5 +7,17 @@ module.exports = {
                port: 7545,
                network_id: "*" // Match any network id
           }
+     },
+     // Configure your compilers
+     compilers: {
+     solc: {
+          version: "0.4.25",    // Fetch exact version from solc-bin (default: truffle's version)
+          settings: {          // See the solidity docs for advice about optimization and evmVersion
+               optimizer: {
+                    enabled: false,
+                    runs: 200
+               }
+          }
      }
+ }
 };
